9182c8b7a8cd0fbb3184eaeb60f2d44e0e94d9c5
Tarek Rahman <44648395+tarekrahman3@users.noreply.github.com>
2023-03-31 11:14:13 +0600

    .update

--- analyser.py
+++ analyser.py
@@ -25,20 +25,20 @@ def analyse_wav(wav_file):
     input_file = wav_file['file_name']
     timestamp = re.search(r" (.+)\.wav", input_file).group(1)
     wav_file['called_time'] = convert_unix_timestamp(float(timestamp))
-    wav_file['phone_number'] = re.search(r"\d+", input_file).group()
-    wav_file['active_status'] = None
+    wav_file['phn_nmbr'] = re.search(r"\d+", input_file).group()
+    wav_file['status'] = None
     _, input_signal = wavfile.read(input_file)
     input_signal = input_signal / np.max(np.abs(input_signal))
-    wav_file['good_correlation'] = get_corr(input_signal, good_samples_signals)
-    wav_file['mute_correlation'] = 0.00
-    if wav_file['good_correlation'] > 5000:
-        wav_file['active_status'] = "Active"
+    wav_file['good_corr'] = get_corr(input_signal, good_samples_signals)
+    wav_file['mute_corr'] = 0.00
+    if wav_file['good_corr'] > 5000:
+        wav_file['status'] = "Active"
     else:
-        wav_file['mute_correlation'] = get_corr(input_signal, mute_samples_signals)
-        if wav_file['mute_correlation'] > 500:
-            wav_file['active_status'] = "Mute"
+        wav_file['mute_corr'] = get_corr(input_signal, mute_samples_signals)
+        if wav_file['mute_corr'] > 500:
+            wav_file['status'] = "Mute"
         else:
-            wav_file['active_status'] = "Inactive"
+            wav_file['status'] = "Inactive"
     return wav_file
 
 def convert_unix_timestamp(timestamp):
